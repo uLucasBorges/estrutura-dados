018cab65a5cc51b2cb26d088e857d14a1593afb6
lucas.avelarb <lucas.avelarb@hotmail.com>
2025-04-09 21:30:25 -0300

    Adicionando main.js ao repositório

---
+++ main.js
@@ -0,0 +1,16 @@
+let aluno_nota_av1 = 8;
+let aluno_nota_av2 = 9.6;
+let aluno_media = (aluno_nota_av1 + aluno_nota_av2) / 2;
+
+console.log("Exemplo com arrays");
+
+let notas_aluno = [8, 9.6];
+console.log(notas_aluno);
+
+console.log("AV1: " + notas_aluno[0]);
+console.log("AV2: " + notas_aluno[1]);
+
+let media_aluno = (notas_aluno[0] + notas_aluno[1]) / 2;
+
+console.log("Média: " + media_aluno);
+console.log("Todas: " + notas_aluno);
